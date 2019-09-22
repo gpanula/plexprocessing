daa49b62ebbb669122e2b42587ad823955f25bbd
G. Panula <git@dev.null>
2019-09-21 19:46:09 -0500

    :bug: bug squashing

--- mythbrake.sh
+++ mythbrake.sh
@@ -25,15 +25,34 @@ mediainfo="$( which mediainfo)"
 cc_track="$( $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Text") | select(.Format=="EIA-608") | ."@typeorder"' | head -n 1 )"
 
 # grab first english audio track
-audio_track="$( $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Text") | select(.Language=="en") | ."@typeorder"' | head -n 1 )"
+audio_track="$( $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Audio") | select(.Language=="en") | ."@typeorder"' | head -n 1 )"
+
+# info
+$mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="General")' >> $log
+$mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Video")' >> $log
+$mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Audio") | select(.Language=="en")' >> $log
+$mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Text") | select(.Format=="EIA-608")' >> $log
 
 # handbrake options
 # ref: https://handbrake.fr/docs/en/latest/cli/command-line-reference.html
-options="-f mp4 -e x264 --x264-preset veryfast --x264-profile high --x264-tune film -q 30 -a ${audio_track} -E copy:ac3 --normalize-mix --large-file --decomb -O -s ${cc_track}"
+# ref: https://handbrake.fr/docs/en/latest/technical/official-presets.html
+if [ ! -z ${cc_track} ]
+then
+  options="-a ${audio_track} -E copy:ac3 --normalize-mix --large-file --decomb -O -s ${cc_track}"
+else
+  options="-a ${audio_track} -E copy:ac3 --normalize-mix --large-file --decomb -O"
+fi
+
+# ref: https://handbrake.fr/docs/en/latest/technical/official-presets.html
+preset="Very Fast 1080p30"
+
+# using flatpak, so we can use the latest version of handbrake
+flatpak="$( which flatpak )"
+handbrakecli="$flatpak --filesystem=/home/mythtv run fr.handbrake.HandBrakeCLI"
 
 # transcode the file
 echo $( date ) >> $log
-nice -n 10 HandBrakeCLI $options -i "${input}" -o "${output}" >> "${log}" 2>&1
+nice -n 10 $handbrakecli -Z "$preset" $options -i "${input}" -o "${output}" >> "${log}" 2>&1
 echo $( date ) >> $log
 
 exit 0
