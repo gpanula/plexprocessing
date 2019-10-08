804505426e1a8e75bf752536a4f822963dd85e17
G. Panula <git@dev.null>
2019-10-07 22:01:20 -0500

    :art: tweaks and comments

--- mythbrake.sh
+++ mythbrake.sh
@@ -45,18 +45,8 @@ $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Text
 $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Video") | .ScanType'  >> $log
 $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Video") | .FrameRate' | cut -c1-5 >> $log
 
-# handbrake options
-# ref: https://handbrake.fr/docs/en/latest/cli/command-line-reference.html
-# ref: https://handbrake.fr/docs/en/latest/technical/official-presets.html
 
-# check for interlaced video
-if [ $( $mediainfo --Output=JSON $input | jq -r '.media.track[] | select(."@type"=="Video") | .ScanType' ) == "Interlaced" ]
-then
-  delace="--deinterlace"
-else
-  delace=""
-fi
-
+# set cc option if we found a cc track
 if [ ! -z ${cc_track} ]
 then
   cc="-s ${cc_track}"
@@ -64,14 +54,16 @@ else
   cc=""
 fi
 
+# handbrake options
+# ref: https://handbrake.fr/docs/en/latest/cli/command-line-reference.html
+# ref: https://handbrake.fr/docs/en/latest/technical/official-presets.html
+
+# * CBS & NBC broadcasts in 1080i30, FOX in 720p60
+# * "Roku 720p30 Surround" is a preset the handles both formats; good picture and **reduces** the file size
 preset="Roku 720p30 Surround"
 
-#static_handbrake_options="-f mp4 -e x264 --x264-preset fast --x264-profile high --x264-tune film -q 22 -E copy:ac3 --normalize-mix --large-file -O --pfr"
 static_handbrake_options='-f mp4 -e x264 -q 26 -E copy:ac3 --normalize-mix --large-file -O --vfr'
-
-#handbrake_options="${static_handbrake_options} -a ${audio_track} -r ${framerate} ${cc} ${delace}"
 handbrake_options="${static_handbrake_options} -a ${audio_track} ${cc}"
-
 
 # using flatpak, so we can use the latest version of handbrake
 flatpak="$( which flatpak )"
@@ -82,23 +74,23 @@ echo $handbrakecli -Z "${preset}" $handbrake_options -i "${input}" -o "${output}
 echo ""
 
 # transcode the file
-echo $( date ) >> $log
+echo "Transcoding started at $( date )" >> $log
 #script -a -c 'nice -n 10 $handbrakecli $handbrake_options -i "${input}" -o "${output}" 2>&1' ${log}
 nice -n 10 $handbrakecli -Z "${preset}" $handbrake_options -i "${input}" -o "${output}" >> ${log} 2>&1
-echo $( date ) >> $log
+echo "Transcodeing completed at$( date )" >> $log
 
-#announce="udp://tracker.yoshi210.com:6969/announce"
 announce="udp://172.27.228.2:6969/announce"
 torrentout="/home/mythtv/torrent/$( echo $OUTFILE | sed 's:mp4:torrent:' )"
 mktorrent=$(which mktorrent)
 
 echo ""
 echo "Now creating torrent file for $output"
-echo "mktorrent -v -a $announce -n $OUTFILE -l 22 -o $torrentout $output"
+
 if [ -z "${mktorrent}" ]
 then
   echo "mktorrent NOT FOUND"
 else
+  echo "mktorrent -v -a $announce -n $OUTFILE -l 22 -o $torrentout $output"
   $mktorrent -v -a $announce -n $OUTFILE -l 22 -o $torrentout $( echo $output | sed 's:mp4:torrent' )
 fi
 
@@ -116,12 +108,12 @@ else
   mysql_options="-u mythtv -pmythtv -s -r -N -D mythconverg"
   subtitle_query="select subtitle from recorded where chanid=$CHANID and basename='$FILE';"
   subtitle=$( $mysql $mysql_options -e "$subtitle_query")
-
+  
   title_query="select title from recorded where chanid=$CHANID and basename='$FILE';"
   title=$( $mysql $mysql_options -e "$title_query")
-
+  
   trimmed_title=$( echo $title | sed 's/FOX NFL //' | sed 's/^.*Football: //' | sed 's/@/at/' )
-
+  
   # if subtitle is empty populate it with bit from title
   # FOX seems to be the only one not populating the subtitle
   if [ ! -z "${subtitle}" ]
@@ -135,14 +127,14 @@ else
     echo updated subtitle is >> "${log}"
     $mysql $mysql_options -e "$subtitle_query" >> "${log}" 2>&1
   fi
-
+  
   # update basename to new file
   $mysql $mysql_options -e "update recorded set basename='$OUTFILE' where chanid=$CHANID and basename='$FILE';" >> "${log}" 2>&1
-
+  
   # update filesize
   filesize=$( ls -l $output | awk '{ print $5 }' )
   $mysql $mysql_options -e "update recorded set filesize='$filesize' where chanid=$CHANID and basename='$OUTFILE';" >> "${log}" 2>&1
-
+  
   $mysql $mysql_options -e "select subtitle,basename,filesize from recorded where chanid=$CHANID and basename='$OUTFILE';"  >> "${log}" 2>&1
 fi
 
